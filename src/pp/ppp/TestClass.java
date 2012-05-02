7bc1dcd469d368d73509a1c5e03628b0fe05cffa
zfadeev <zahar.fadeev@shapeservices.com>
2012-05-02 14:08:41 +0600

    1

---
+++ src/pp/ppp/TestClass.java
@@ -0,0 +1,11 @@
+package pp.ppp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zakhar
+ * Date: 02.05.12
+ * Time: 14:08
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TestClass {
+}
